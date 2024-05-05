4b51cd2229b199d0597cb817080ff87ba9510fea
LukiLA1996 <lukila1996@gmail.com>
2024-05-05 14:24:58 +0700

    add endpoint view category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryControllers');
 
-router.get('/', categoryController.getAllCategories);
+router.get('/view', categoryController.getAllCategories);
 router.post('/', categoryController.addCategory);
 router.put('/:id', categoryController.updateCategory);
 router.delete('/:id', categoryController.deleteCategory);
