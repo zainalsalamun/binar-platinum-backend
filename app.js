7903deb8a19163737bbf58bf5814b755ebeaf43d
binar-rizki <zainalrtf@gmail.com>
2024-05-08 10:53:06 +0700

    update app

--- app.js
+++ app.js
@@ -147,7 +147,6 @@ app.post('/api/photo', upload.single('photo'), async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server = app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+})
